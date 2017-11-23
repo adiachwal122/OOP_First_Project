11aaa30c7374b2b36d18c6f1bc4f6baa3274ffd6
Adiel Matuf <matufadiel@gmail.com>
2017-11-23 23:46:33 +0200

    Update kml

--- First_project/src/projet00/WriteKml.java
+++ First_project/src/projet00/WriteKml.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import de.micromata.opengis.kml.v_2_2_0.Coordinate;
+import de.micromata.opengis.kml.v_2_2_0.Document;
 import de.micromata.opengis.kml.v_2_2_0.Kml;
 import de.micromata.opengis.kml.v_2_2_0.KmlFactory;
 import de.micromata.opengis.kml.v_2_2_0.Placemark;
@@ -33,13 +34,15 @@ public class WriteKml {
 	 * @param keyIndex {@link https://labs.micromata.de/projects/jak/kml-in-the-java-world.html}
 	 * @return Kml file
 	 */
+
 	public WriteKml(List<String []> kmlList ,HashMap<String, Integer> keyIndex){
-		try {
+		/*try {
 		// The all encapsulating kml element.
 		Kml kml = KmlFactory.createKml();
 		for (String[] list : kmlList) {
 			if(list != null){
 				//Document doc = KmlFactory.createDocument();
+				
 				// Create <Placemark> and set values.
 				Placemark placemark = KmlFactory.createDocument().withName(list[keyIndex.get("SSID")]).createAndAddPlacemark();
 				//doc.addToFeature(placemark);
@@ -72,21 +75,23 @@ public class WriteKml {
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}   
-		/*try {
+		}  */ 
+		try {
 			List<String []> listOfNet = kmlList;
 			final Kml writekml = new Kml();
+			Document document = writekml.createAndSetDocument();
+
 			for (String[] list : listOfNet) {
 				if(list != null)
-				writekml.createAndSetDocument().withName(list[keyIndex.get("SSID")]).createAndAddPlacemark()
-				.withName(list[keyIndex.get("SSID")]).withId(list[keyIndex.get("ID")]).withDescription("MAC: " 
-				+ list[keyIndex.get("MAC")] +
-						"\nFrequncy: " + list[keyIndex.get("Frequncy")] + 
-						"\nSignal: " + list[keyIndex.get("Signal")])
-				.withOpen(Boolean.TRUE).createAndSetPoint()
-				.addToCoordinates(Double.parseDouble(list[keyIndex.get("Lat")])
-						, Double.parseDouble(list[keyIndex.get("Lon")]) 
-						, Double.parseDouble(list[keyIndex.get("Alt")]));
+					document.createAndAddDocument().withName(list[keyIndex.get("SSID")]).createAndAddPlacemark()
+					.withName(list[keyIndex.get("SSID")]).withId(list[keyIndex.get("ID")]).withDescription("MAC: " 
+							+ list[keyIndex.get("MAC")] +
+							"\nFrequncy: " + list[keyIndex.get("Frequncy")] + 
+							"\nSignal: " + list[keyIndex.get("Signal")])
+					.withOpen(Boolean.TRUE).createAndSetPoint()
+					.addToCoordinates(Double.parseDouble(list[keyIndex.get("Lat")])
+							, Double.parseDouble(list[keyIndex.get("Lon")]) 
+							, Double.parseDouble(list[keyIndex.get("Alt")]));
 			}
 			
 			writekml.marshal(new File("KmlFile.kml"));
@@ -97,13 +102,11 @@ public class WriteKml {
 		}catch (NullPointerException e) {
 			// TODO Auto-generated catch block
 			System.out.println(e.getMessage());
-		}*/
+		}
 	}
 	
 	/**
 	 * Unauthorized file.
-	 *
-	 * @param unauthorizedFile the unauthorized file
 	 */
 	//Check if file type is unauthorized 
 	public void unauthorizedFile(String unauthorizedFile) {

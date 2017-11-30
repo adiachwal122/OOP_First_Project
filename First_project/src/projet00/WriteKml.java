afc6b777ce773e79de40ce78fd4cf6d8bb75856d
Adiel <32271159+MatufA@users.noreply.github.com>
2017-11-30 14:57:24 +0200

    Update TimeStamp

--- First_project/src/projet00/WriteKml.java
+++ First_project/src/projet00/WriteKml.java
@@ -5,12 +5,9 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.List;
 
-import de.micromata.opengis.kml.v_2_2_0.Coordinate;
 import de.micromata.opengis.kml.v_2_2_0.Document;
 import de.micromata.opengis.kml.v_2_2_0.Kml;
-import de.micromata.opengis.kml.v_2_2_0.KmlFactory;
 import de.micromata.opengis.kml.v_2_2_0.Placemark;
-import de.micromata.opengis.kml.v_2_2_0.Point;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -40,19 +37,21 @@ public class WriteKml {
 			List<String []> listOfNet = kmlList;
 			final Kml writekml = new Kml();
 			Document document = writekml.createAndSetDocument();
-
 			for (String[] list : listOfNet) {
-				if(list != null)
-					document.createAndAddDocument().withName(list[keyIndex.get("SSID")]).createAndAddPlacemark()
-					.withName(list[keyIndex.get("SSID")]).withId(list[keyIndex.get("ID")]).withDescription("MAC: " 
+
+				if(list != null) {
+				Placemark placemark = document.createAndAddPlacemark()
+					.withName(list[keyIndex.get("SSID")]).withOpen(Boolean.TRUE);
+				placemark.withId(list[keyIndex.get("ID")]).withDescription("MAC: " 
 							+ list[keyIndex.get("MAC")] +
 							"\nFrequncy: " + list[keyIndex.get("Frequncy")] + 
-							"\nSignal: " + list[keyIndex.get("Signal")])
-					.withOpen(Boolean.TRUE).createAndSetPoint()
+							"\nSignal: " + list[keyIndex.get("Signal")]).createAndSetPoint()
 					.addToCoordinates(Double.parseDouble(list[keyIndex.get("Lon")])
 							, Double.parseDouble(list[keyIndex.get("Lat")]) 
 							, Double.parseDouble(list[keyIndex.get("Alt")]));
-				document.createAndSetTimeStamp().withWhen(list[keyIndex.get("Time")]);
+				placemark.createAndSetTimeStamp().withWhen(timeStampFormate(list[keyIndex.get("Time")]));			
+				}
+				
 			}
 			
 			writekml.marshal(new File("KmlFile.kml"));
@@ -64,6 +63,16 @@ public class WriteKml {
 			// TODO Auto-generated catch block
 			System.out.println(e.getMessage());
 		}
+	}
+	/*
+	 * Change formate for TimeStamp 
+	 * */
+	public String timeStampFormate(String time) {
+		String[] getTime = time.split("\\D");
+		String newTimeFormate = getTime[2] + "-" + getTime[1] + "-" +getTime[0] + "T" + 
+		getTime[3] + ":" + getTime[4] + ":00" + "Z";
+		
+		return newTimeFormate;
 	}
 	
 	/**
